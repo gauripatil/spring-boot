af99900874d87e018aed561213c58b14a6bea903
Gauri Patil <gauri.be0811@gmail.com>
2023-05-26 20:38:29 +0530

    Build Hello World API with Spring Boot

---
+++ learn-spring-boot/src/main/java/com/mylearning/learnspringboot/LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
