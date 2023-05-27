4533efd870bd5c99c50efaaf1e9d3150f4766ea9
Gauri Patil <gauri.be0811@gmail.com>
2023-05-27 21:04:26 +0530

    Understanding the Goals of Spring Boot

--- learn-spring-boot/src/main/java/com/mylearning/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/mylearning/learnspringboot/LearnSpringBootApplication.java
@@ -11,3 +11,20 @@ public class LearnSpringBootApplication {
 	}
 
 }
+
+
+/**
+ * IMPORTANT GOALS OF SPRING BOOT
+ *
+ * Help you build PRODUCTION READY apps QUICKLY
+ * 		1. Build QUICKLY
+ * 			1.1 Spring Initialzr
+ * 			1.2 Spring Boot Starter Project
+ * 			1.3 Spring Boot Auto Configuration
+ * 			1.4 Spring Boot DevTools
+ * 		2. Be PRODUCTION READY
+ * 			2.1 Logging
+ * 			2.2 Different Configuration for Different Environments
+ * 				2.2.1 Profiles, ConfigurationProperties
+ * 			2.3 Monitoring (Spring Boot Actuator)
+ */
