8f936ed350dbbf864eedf258dd6ed208750a9ea1
Gauri Patil <gauri.be0811@gmail.com>
2023-06-03 03:06:37 +0530

    comment added

--- myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/login/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    // "/login" => login.jsp
+    // /src/main/META-INF/resources/WEB-INF/jsp/login.jsp
     @RequestMapping("login")
     public String doLogin() {
         return "login";
