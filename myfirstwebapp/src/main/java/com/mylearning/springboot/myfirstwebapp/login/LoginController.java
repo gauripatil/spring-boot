1b2508e31a57cf051497540003dd0ec55827b9ec
Gauri Patil <gauri.be0811@gmail.com>
2023-06-26 23:54:05 +0530

    removed login authentication service

--- myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/login/LoginController.java
@@ -10,39 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @SessionAttributes("yourName")
 public class LoginController {
 
-    AuthenticationService authenticationService;
     private Logger logger;
-    public LoginController(AuthenticationService authenticationService) {
-        super();
-        this.logger = LoggerFactory.getLogger(this.getClass());
-        this.authenticationService = authenticationService;
-
-    }
-
-
 
     // "/login" => login.jsp
     // /src/main/META-INF/resources/WEB-INF/jsp/login.jsp
-    @RequestMapping(value="login", method= RequestMethod.GET)
-    public String doLogin() {
-        return "login";
-    }
-
-    @RequestMapping(value="login", method= RequestMethod.POST)
-    public String goToWelcome(@RequestParam String name, @RequestParam String password,
-                              ModelMap model) {
-
-        if(authenticationService.authenticate(name, password)) {
-            System.out.println("Welcome Page");
-
-            model.put("yourName", name);
-            model.put("yourPassword", password);
-            return "welcome";
-        }
-
-        model.put("errorMsg", "Invalid Credentials!!!");
-        return "login";
-
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String goToWelcomePage(ModelMap model) {
+        model.put("name", "Gauri");
+        return "welcome";
     }
 
 }
