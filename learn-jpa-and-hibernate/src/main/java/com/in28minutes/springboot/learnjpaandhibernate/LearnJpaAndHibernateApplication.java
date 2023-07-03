7b98bb7eec7acbf675a13fdc2be9e4e063844ab9
Gauri Patil <gauri.be0811@gmail.com>
2023-07-03 22:36:33 +0530

    Setting up new project for jpa & hibernate. launching up h2 console & table creation in h2

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplication.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.learnjpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// http://localhost:8080/h2-console
+
+@SpringBootApplication
+public class LearnJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+	}
+
+}
