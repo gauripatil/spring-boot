fdf4eaa26d10aa7012a543458009df5cac8487ec
Gauri Patil <gauri.be0811@gmail.com>
2023-07-04 10:30:39 +0530

    Removed hard coding and insert, delete queries implemented

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,10 +12,19 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY =
     """
         insert into course (id, name, author)
-        values (1, 'Learn AWS', 'Gauri');
+        values (?, ?, ?);
     """;
 
-    public void insert() {
-        springJdbcTemplate.update(INSERT_QUERY);
+    private static String DELETE_QUERY =
+            """
+                delete from course where id=?;
+            """;
+
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void delete(Course course) {
+        springJdbcTemplate.update(DELETE_QUERY, course.getId());
     }
 }
