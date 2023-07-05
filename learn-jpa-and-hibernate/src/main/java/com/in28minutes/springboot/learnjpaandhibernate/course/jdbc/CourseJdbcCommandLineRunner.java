2305eccccc8f7cf503f3fc944cda7df1ece76a1e
Gauri Patil <gauri.be0811@gmail.com>
2023-07-05 06:09:28 +0530

    Getting Started with JPA & entityManger

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,21 +1,25 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+//    @Autowired
+//    private CourseJdbcRepository repository;
+
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJpaRepository repository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Now!!", "Gauri"));
-        repository.insert(new Course(2, "Learn Java Now!!", "Gauri"));
-        repository.insert(new Course(3, "Learn React Now!!", "Gauri"));
+        repository.insert(new Course(1, "Learn AWS Jpa!!", "Gauri"));
+        repository.insert(new Course(2, "Learn Java Jpa!!", "Gauri"));
+        repository.insert(new Course(3, "Learn React Jpa!!", "Gauri"));
 
         repository.deleteById(1);
         System.out.println(repository.selectById(2));
