239254bf687b6098d66a2af7d7bc49b986702b3f
Gauri Patil <gauri.be0811@gmail.com>
2023-07-06 22:47:29 +0530

    Getting started with Spring Data JPA

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdata.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,29 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Jpa!!", "Gauri"));
-        repository.insert(new Course(2, "Learn Java Jpa!!", "Gauri"));
-        repository.insert(new Course(3, "Learn React Jpa!!", "Gauri"));
+//        repository.insert(new Course(1, "Learn AWS Jpa!!", "Gauri"));
+//        repository.insert(new Course(2, "Learn Java Jpa!!", "Gauri"));
+//        repository.insert(new Course(3, "Learn React Jpa!!", "Gauri"));
+//
+//        repository.deleteById(1);
+//        System.out.println(repository.selectById(2));
+//        System.out.println(repository.selectById(3));
+
+        repository.save(new Course(1, "Learn AWS - Spring Data Jpa!!", "Gauri"));
+        repository.save(new Course(2, "Learn Java - Spring Data Jpa!!", "Gauri"));
+        repository.save(new Course(3, "Learn React - Spring Data Jpa!!", "Gauri"));
 
         repository.deleteById(1);
-        System.out.println(repository.selectById(2));
-        System.out.println(repository.selectById(3));
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     }
 }
