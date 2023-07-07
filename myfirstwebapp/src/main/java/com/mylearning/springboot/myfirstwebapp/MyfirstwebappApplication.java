3f50f7e000a98664ee70bcf1418a77a6960d8cea
Gauri Patil <gauri.be0811@gmail.com>
2023-07-07 17:08:10 +0530

    Adding Spring Boot Starter Data JPA & Getting Started with H2 database

--- myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/MyfirstwebappApplication.java
+++ myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyfirstwebappApplication {
 	public static void main(String[] args) {
+
+		// http://localhost:8080/h2-console
 		SpringApplication.run(MyfirstwebappApplication.class, args);
 	}
 }
