765d0a41f8c20b3a9443aad37691e55de9ddaa20
Gauri Patil <gauri.be0811@gmail.com>
2023-07-08 16:02:00 +0530

    Configuring Spring Security to Get H2 Console running

--- myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -3,13 +3,17 @@ package com.mylearning.springboot.myfirstwebapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -40,6 +44,23 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    // All URLs are protected
+    // A login form is shown for all unauthorized requests
+    // CSRF disable
+    // Frames
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 
 }
