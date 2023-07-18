30dd91fcb09dacd6ebd1224e96f4ba496ceb2f09
Gauri Patil <gauri.be0811@gmail.com>
2023-07-18 23:57:29 +0530

    Making ToDo an entity and Population Todo data into H2

--- myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/todo/ToDo.java
+++ myfirstwebapp/src/main/java/com/mylearning/springboot/myfirstwebapp/todo/ToDo.java
@@ -1,10 +1,16 @@
 package com.mylearning.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class ToDo {
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
 
---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into TO_DO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'gauri', 'AWS Course', CURRENT_DATE(), false);
+
+insert into TO_DO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'gauri', 'GCP Course', CURRENT_DATE(), false);
+
+insert into TO_DO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'gauri', 'Azure Course', CURRENT_DATE(), false);
+
+insert into TO_DO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'gauri', 'React Course', CURRENT_DATE(), false);
+
+insert into TO_DO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10005, 'gauri', 'React Native Course', CURRENT_DATE(), false);
+
+select * from TO_DO
