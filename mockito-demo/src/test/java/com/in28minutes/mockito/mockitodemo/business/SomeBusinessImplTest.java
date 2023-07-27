2ddcb4a56cd0a92b47153863724875c82515b511
Gauri Patil <gauri.be0811@gmail.com>
2023-07-27 23:53:40 +0530

    Added test file for service

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -0,0 +1,13 @@
+package com.in28minutes.mockito.mockitodemo.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeBusinessImplTest {
+
+    @Test
+    void findGreatestFromAllData() {
+        fail("Not yet implemented");
+    }
+}
