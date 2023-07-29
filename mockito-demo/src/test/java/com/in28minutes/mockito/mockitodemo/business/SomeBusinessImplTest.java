c78311ca3e1446a82f567ef42e93eef6081025cd
Gauri Patil <gauri.be0811@gmail.com>
2023-07-29 22:49:57 +0530

    Problem testing with stub - we have to create multiple classes for different data combinations

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImplTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -13,6 +13,14 @@ class SomeBusinessImplTest {
         int result = businessImpl.findGreatestFromAllData();
         assertEquals(25, result);
     }
+
+    @Test
+    void findGreatestFromAllData_one_parameter() {
+        DataServiceStub2 dataServiceStub2 = new DataServiceStub2();
+        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub2);
+        int result = businessImpl.findGreatestFromAllData();
+        assertEquals(35, result);
+    }
 }
 
 class DataServiceStub implements DataService {
@@ -20,5 +28,13 @@ class DataServiceStub implements DataService {
     @Override
     public int[] retrieveAllData() {
         return new int[]{25, 15, 5};
+    }
+}
+
+class DataServiceStub2 implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{35};
     }
 }
