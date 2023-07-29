67423c103ebd7c0000a8022e4f0cfbfbf280dcf2
Gauri Patil <gauri.be0811@gmail.com>
2023-07-30 01:49:07 +0530

    Simplifying Tests with Mockito extensions - @Mocks, @InjectMocks

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImplMockTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImplMockTest.java
@@ -1,28 +1,41 @@
 package com.in28minutes.mockito.mockitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+
+    @Mock
+    private DataService dataService;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
 
     @Test
     void findGreatestFromAllData_basic() {
-        DataService dataService = mock(DataService.class);
-//        dataService.retrieveAllData() => new int[]{25, 15, 5};
         when(dataService.retrieveAllData()).thenReturn(new int[] {25, 15, 5});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataService);
         assertEquals(25, businessImpl.findGreatestFromAllData());
     }
 
     @Test
-    void findGreatestFromAllData_one_value() {
-        DataService dataService = mock(DataService.class);
+    void findGreatestFromAllData_oneValue() {
         when(dataService.retrieveAllData()).thenReturn(new int[] {35});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataService);
         assertEquals(35, businessImpl.findGreatestFromAllData());
+    }
+
+    @Test
+    void findGreatestFromAllData_emptyArray() {
+        when(dataService.retrieveAllData()).thenReturn(new int[] {});
+        assertEquals(Integer.MIN_VALUE, businessImpl.findGreatestFromAllData());
     }
 
 }
