8567845bedd7807fc5a024225c89f9e36f5ea7c7
Gauri Patil <gauri.be0811@gmail.com>
2023-08-07 01:24:20 +0530

    Exploring Path Variables & Path Params

--- first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/helloworld/HelloWorldResource.java
+++ first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/helloworld/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package com.mylearning.sprintgboot.firstrestapi.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,18 @@ public class HelloWorldResource {
     @RequestMapping("/hello-world-bean")
     public HelloWorldResourceBean sayHelloWorldBean() {
         return new HelloWorldResourceBean("Hello World");
+    }
+
+    // Path Variables or Path params
+    // /user/Ranga/todos/1
+    @RequestMapping("/hello-world-path-param/{name}")
+    public HelloWorldResourceBean sayHelloWorldPathParams(@PathVariable String name) {
+        return new HelloWorldResourceBean("Hello World, " + name);
+    }
+
+    @RequestMapping("/hello-world-path-param/{name}/message/{message}")
+    public HelloWorldResourceBean sayHelloWorldMultiplePathParams(@PathVariable String name,
+                              @PathVariable String message) {
+        return new HelloWorldResourceBean("Hello World, " + name + ", " + message);
     }
 }
