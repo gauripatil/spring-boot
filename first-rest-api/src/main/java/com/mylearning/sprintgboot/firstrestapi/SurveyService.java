cb088f0b60c331807172d6ed73c879cf38cf5118
Gauri Patil <gauri.be0811@gmail.com>
2023-08-10 00:17:44 +0530

    Get specific survey based on surveyid

--- first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/SurveyService.java
+++ first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/SurveyService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Service
 public class SurveyService {
@@ -36,5 +38,12 @@ public class SurveyService {
 
     public List<Survey> retrieveAllSurveys() {
         return surveys;
+    }
+
+    public Survey getSurveyById(String surveyId) {
+        Predicate<? super Survey> predicate = survey -> survey.getId().equalsIgnoreCase(surveyId);
+        Optional<Survey> optionalSurvey = surveys.stream().filter(predicate).findFirst();
+        if(optionalSurvey.isEmpty()) return null;
+        return optionalSurvey.get();
     }
 }
--- first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResource.java
+++ first-rest-api/src/main/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResource.java
@@ -1,8 +1,12 @@
 package com.mylearning.sprintgboot.firstrestapi.survey;
 
 import com.mylearning.sprintgboot.firstrestapi.SurveyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,5 +23,15 @@ public class SurveyResource {
     @RequestMapping("/surveys")
     public List<Survey> retrieveAllSurveys() {
         return surveyService.retrieveAllSurveys();
+    }
+
+    // /surveys => surveys
+    @RequestMapping("/surveys/{surveyId}")
+    public Survey getSurveyById(@PathVariable String surveyId) {
+        Survey survey = surveyService.getSurveyById(surveyId);
+
+        if(survey==null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return survey;
     }
 }
