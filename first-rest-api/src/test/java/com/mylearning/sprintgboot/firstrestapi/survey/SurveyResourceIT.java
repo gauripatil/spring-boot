fe974217db72480b7c59aec21c0a14056f6b3018
Gauri Patil <gauri.be0811@gmail.com>
2023-08-28 00:44:13 +0530

    Writing asserts for JSON - JSONAssert

--- first-rest-api/src/test/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResourceIT.java
+++ first-rest-api/src/test/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResourceIT.java
@@ -1,6 +1,8 @@
 package com.mylearning.sprintgboot.firstrestapi.survey;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -10,22 +12,32 @@ import org.springframework.http.ResponseEntity;
 public class SurveyResourceIT {
 
     // http://localhost:8080/surveys/survey1/questions/Question1
-    String str = """
-                
-            """;
+
 
     private static String SPECIFIC_URL_STR = "/surveys/survey1/questions/Question1";
     @Autowired
     TestRestTemplate template;
 
     @Test
-    void retrieveQuestionById_basicScenario() {
+    void retrieveQuestionById_basicScenario() throws JSONException {
         ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_URL_STR, String.class);
         System.out.println(responseEntity.getBody());
-        // {"id":"Question1","description":"Most Popular Cloud Platform Today","correctAnswer":"AWS","option":["AWS","Azure","Google Cloud","Oracle Cloud"]}
+        String expectedResponseStr = """
+                {
+                    "id":"Question1",
+                    "description":"Most Popular Cloud Platform Today",
+                    "correctAnswer":"AWS",
+                    "option":["AWS","Azure","Google Cloud","Oracle Cloud"]
+                }
+            """;
+        //
 
         System.out.println(responseEntity.getHeaders());
         // [Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Sat, 26 Aug 2023 16:16:43 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]
+
+        JSONAssert.assertEquals(expectedResponseStr, responseEntity.getBody(), false);
+
+
 
     }
 }
