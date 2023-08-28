d138017c437ffa1951447531ce8b25dd491ddf89
Gauri Patil <gauri.be0811@gmail.com>
2023-08-28 23:57:38 +0530

    Writing Spring Boot Integration Test for GET method returning List

--- first-rest-api/src/test/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResourceIT.java
+++ first-rest-api/src/test/java/com/mylearning/sprintgboot/firstrestapi/survey/SurveyResourceIT.java
@@ -16,6 +16,7 @@ public class SurveyResourceIT {
 
     // http://localhost:8080/surveys/survey1/questions/Question1
     private static String SPECIFIC_URL_STR = "/surveys/survey1/questions/Question1";
+    private static String ALL_QUESTIONS_URL_STR = "/surveys/survey1/questions";
     @Autowired
     TestRestTemplate template;
 
@@ -31,6 +32,38 @@ public class SurveyResourceIT {
                     "option":["AWS","Azure","Google Cloud","Oracle Cloud"]
                 }
             """;
+        //
+
+        System.out.println(responseEntity.getHeaders());
+        // [Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Sat, 26 Aug 2023 16:16:43 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]
+
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponseStr, responseEntity.getBody(), false);
+
+
+
+
+    }
+
+    @Test
+    void getAllSurveyQuestions_basicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(ALL_QUESTIONS_URL_STR, String.class);
+        System.out.println(responseEntity.getBody());
+        String expectedResponseStr = """
+                        [
+                             {
+                                 "id": "Question1",
+                             },
+                             {
+                                 "id": "Question2",
+                             },
+                             {
+                                 "id": "Question3",
+                             }
+                         ]
+                         
+                """;
         //
 
         System.out.println(responseEntity.getHeaders());
