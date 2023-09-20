2d7fab5af8789c09bc122afecf828bdfd53aec4b
Gauri Patil <gauri.be0811@gmail.com>
2023-09-20 23:58:29 +0530

    FP - practice

--- functional-programming/src/FP01Functional.java
+++ functional-programming/src/FP01Functional.java
@@ -2,17 +2,37 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args) {
-        System.out.println("All Numbers:");
+        System.out.println("\nAll Numbers:");
         List<Integer> integers = List.of(12, 34, 5, 6, 7, 123, 556);
         printAllNumbersInListFunctional(integers);
 
-        System.out.println("Even Numbers:");
+        System.out.println("\nEven Numbers:");
         printEvenNumbersInListFunctional(integers);
 
 
-        System.out.println("Odd Numbers:");
+        System.out.println("\nOdd Numbers:");
         printOddNumbersInListFunctional(integers);
+
+        System.out.println("\nAll Courses : ");
+        List<String> courses = List.of("Spring", "Java", "Spring Boot", "Nodejs", "ExpressJs", "React", "Angular");
+        printCoursesList(courses);
+
+        System.out.println("\nOnly Spring : ");
+        printCoursesListWithSpring(courses);
     }
+
+    private static void printCoursesListWithSpring(List<String> courses) {
+        courses.stream()
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);
+    }
+
+    private static void printCoursesList(List<String> courses) {
+        courses.stream()
+                .forEach(System.out::println);
+    }
+
+
     private static void printAllNumbersInListFunctional(List<Integer> integers) {
         //[12,34,5,6,7,123,556]
         // Convert number to stream ie
